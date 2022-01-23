4516bd406487b304f3983fbda24c67e5cf936a56
Merwyn Patrick <merwynpatrick@Merwyns-Mac-mini.local>
2022-01-23 13:40:10 -0500

    add table data in seeds

---
+++ seeds/04_seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO property_reviews (guest_id, property_id, reservation_id, rating, message)
+VALUES (2, 5, 10, 3, 'messages'),
+(1, 4, 1, 4, 'messages'),
+(8, 1, 2, 4, 'messages'),
+(4, 2, 7, 5, 'messages'),
+(4, 3, 4, 4, 'messages'),
+(5, 6, 3, 5, 'messages');
